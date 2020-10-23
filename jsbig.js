21ec74a32a6091c250bf6e49a557fc577594cda1
saiie5 <saimavuduru4@gmail.com>
2020-10-23 15:33:07 +0530

    js intro practice

---
+++ jsbig.js
@@ -0,0 +1,37 @@
+var a = "masai school";
+console.log (a);
+
+
+var name = "saikumarM"
+console.log(name);
+
+
+name = "ganeshM";
+console.log(name);
+
+
+var side = 5;var parameter =4*side;
+console .log (parameter);
+
+var x= 10;var y = 20;var z =30;
+console.log (x*y*z);
+
+var n = 2;
+console.log (n**3);
+
+var l = "45";var k = "56";
+l = Number (l);
+k = Number (k)
+console .log (l-k);
+
+var l = 50;
+console.log(l%3);
+ 
+
+var clothes = 10000;
+var food = 2000;
+var rent = 1000;
+var total = food+rent+clothes;
+console.log (total)
+
+
